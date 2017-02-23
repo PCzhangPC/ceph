196ca83dc29dbdd583ad929c3dbe0e6d1e0265ad
Dongsheng Yang <dongsheng.yang@easystack.cn>
2017-02-23 18:22:29 +0800

    rbd: introduce --src-namespace and --dst-namespace in rbd copy/clone command
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

--- src/tools/rbd/Utils.h
+++ src/tools/rbd/Utils.h
@@ -106,7 +106,8 @@ std::string get_default_pool_name();
 std::string get_pool_name(const boost::program_options::variables_map &vm,
                           size_t *arg_index);
 
-std::string get_namespace(const boost::program_options::variables_map &vm);
+std::string get_namespace(const boost::program_options::variables_map &vm,
+			  argument_types::ArgumentModifier mod = argument_types::ARGUMENT_MODIFIER_SOURCE);
 
 int get_pool_image_snapshot_names(
     const boost::program_options::variables_map &vm,
